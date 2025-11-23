01d281e258f779c18b4392dad57c9d98255e7d55
Yash Ladlapure <yash.ladlapure@gmail.com>
2025-11-23 14:50:52 +0530

    Fix indentation in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/meme-vault/',
+  base: '/meme-vault/',
   plugins: [react()],
 })
